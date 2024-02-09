5cdc926a45f954987399beec9d9289bc83a2c364
AKSTHIRUMURUGAN <85742080+AKSTHIRUMURUGAN@users.noreply.github.com>
2024-02-09 17:58:55 +0530

    Update order.js

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -2,12 +2,12 @@ const express=require("express");
 const { newOrder, getSingleOrder, myOrders, orders, updateOrder, deleteOrder } = require("../controllers/orderController");
 const { isAuthenticatedUser, authorizeRole } = require("../middleware/authenticate");
 const router=express.Router();
-router.route('/order/new').post(isAuthenticatedUser,newOrder);
-router.route('/order/:id').get(isAuthenticatedUser,getSingleOrder)
-router.route('/myorders').get(isAuthenticatedUser,myOrders);
+router.route('/order/new').post(newOrder);
+router.route('/order/:id').get(getSingleOrder)
+router.route('/myorders').get(myOrders);
 
 //Admin Routes
-router.route("/admin/orders").get(isAuthenticatedUser,authorizeRole('admin'),orders)
-router.route("/admin/order/:id").put(isAuthenticatedUser,authorizeRole('admin'),updateOrder)
-                          .delete(isAuthenticatedUser,authorizeRole('admin'),deleteOrder)
+router.route("/admin/orders").get(authorizeRole('admin'),orders)
+router.route("/admin/order/:id").put(authorizeRole('admin'),updateOrder)
+                          .delete(authorizeRole('admin'),deleteOrder)
 module.exports=router;
