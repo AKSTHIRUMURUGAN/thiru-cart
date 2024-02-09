1d64bf5c381d1aaf439052b2f960a8beeac49c4a
AKSTHIRUMURUGAN <85742080+AKSTHIRUMURUGAN@users.noreply.github.com>
2024-02-09 16:58:57 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,12 @@ const errorMiddleware=require("./middleware/error")
 const cookieParser=require("cookie-parser")
 const path=require('path')
 const dotenv=require("dotenv");
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://thirucart.netlify.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 dotenv.config({path:path.join(__dirname,"config/config.env")})
 app.use(express.json());
 app.use(cookieParser());
